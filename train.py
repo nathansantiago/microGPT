087a5771a0173c5554c601045b9868075d72ce3a
Nathan Santiago <santiagonathan9254@gmail.com>
2025-01-22 19:49:01 -0500

    Implement data processing and batching for training and validation in train.py

--- train.py
+++ train.py
@@ -1,3 +1,5 @@
+import torch
+
 with open('input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
@@ -5,3 +7,26 @@ with open('input.txt', 'r', encoding='utf-8') as f:
 chars = sorted(list(set(text)))
 vocab_size = len(chars)
 
+# create a mapping from characters to integers
+stoi = { ch:i for i,ch in enumerate(chars) }
+itos = { i:ch for i,ch in enumerate(chars) }
+encode = lambda s: [stoi[c] for c in s]
+decode = lambda l: ''.join([itos[i] for i in l])
+
+data = torch.tensor(encode(text), dtype=torch.long)
+
+# split data into training and validation
+n = int(.9*len(data))
+train_data = data[:n]
+val_data = data[n:]
+
+torch.manual_seed(1337)  # TODO: Remove
+batch_size = 4
+block_size = 8
+
+def get_batch(split):
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,)) # find batch_size number of random blocks 
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    return x, y
